01a853bc3f83ec480f6d056435acaa2c45cf606e
Ivelina <ivs.yordanova@gmail.com>
2019-05-25 17:07:21 +0100

    attempt at fixing the gapi auth - works in opera, not chrome

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -3,56 +3,58 @@ import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component {
+    signInSuccess
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '597723345482-fu6q5sv4nr9s9oqnbvc04fbc5abemau9.apps.googleusercontent.com',
                 scope: 'email'
-            }).then(() => {
+            }).then((a) => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.onAuthChange(this.auth.isSignedIn.get());
-                this.auth.isSignedIn.listen((A) => this.onAuthChange(A));
+                this.auth.attachClickHandler(
+                    document.getElementById('signInButton'),
+                    {},
+                    (googleUser) => {
+                      this.currentUser = googleUser;
+                      this.onAuthChange(true);
+                    },
+                    (error) => {
+                      console.error(error);
+                    }
+                );
+                this.onAuthChange(this.currentUser);
             });
         });
     }
 
-    onSignIn = () => {
-        this.auth.signIn();
-    }
-
-    onSignOut = () => {
-        this.auth.signOut();
-    }
-
     onAuthChange = (isSignedIn) => {
-        console.log(isSignedIn)
         if (isSignedIn) {
-            this.props.signIn(this.auth.currentUser.get().getId());
+            this.props.signIn(this.currentUser.getId());
         } else {
             this.props.signOut();
         }
     }
 
+    signOut = (e) => {
+        e.preventDefault();
+        this.auth.signOut();
+        this.onAuthChange(false);
+    }
+
     renderAuthButton() {
-        if (this.props.isSignedIn === null) {
-            return null;
-        }
-        if (this.props.isSignedIn) {
-            return (
-                <button onClick={ this.onSignOut } className="ui red google button">
-                 <i className="google icon"/>
-                 Sign out
+        return (
+            <div>
+                <button id="signInButton" className="ui red google button" style={{display: `${!this.props.isSignedIn ? 'inline-block' : 'none'}`}}>
+                    <i className="google icon"/>
+                    Sign in with Google
                 </button>
-             );
-        }
-        if (!this.props.isSignedIn) {
-            return (
-              <button onClick={ this.onSignIn } className="ui red google button">
-               <i className="google icon"/>
-               Sign in with Google
-              </button>
-           );
-        }
+                <button id="signOutButton" className="ui red google button" onClick={ this.signOut } style={{display: `${this.props.isSignedIn ? 'inline-block' : 'none'}`}}>
+                    <i className="google icon"/>
+                    Sign out
+                </button>
+            </div>
+         );
     }
 
     render() {
